8c3a24a5a816500de4b95ccab9dddc0377ca6ca9
Sebastian S <suciu.seb@gmail.com>
2024-02-13 10:22:40 +0200

    password encryption

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {body, validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = 3000;
@@ -15,29 +16,35 @@ app.post('/register',
   body('lastName').notEmpty().withMessage('Last name is required.'),
   body('email').isEmail().withMessage('Invalid email address.'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long.'),
-],
-(req, res) => {
+], async (req, res) => {
+
   const {firstName, lastName, email, password} = req.body;
   const errors = validationResult(req);
-  
+
   if(!errors.isEmpty()) {
     return res.status(400).json({errors:errors.array()})
   }
   
   if (users.find(user => user.email === email)) {
-    return res.status(400).json({ message: 'Email is already exists.' });
+    return res.status(400).json({ errors: [{msg: 'Email already exists.'}] });
   }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    };
   
-  const newUser = {
-    firstName,
-    lastName,
-    email,
-    password
-  };
-
-  users.push(newUser);
-
-  return res.status(201).json({ message: 'User registered successfully.' });
+    users.push(newUser);
+  
+    return res.status(201).json({ message: 'User registered successfully.' });
+  } catch {
+    console.error('Error hashing password:', error);
+    return res.status(500).json({ errors: [{msg:'Internal server error.' }]});
+  }
 })
 
 app.get('/users', (req, res) => {
